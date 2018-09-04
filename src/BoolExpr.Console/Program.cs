e23a4fd0f9b129d50dad56a660e3aa3ab3385661
Jerome Haltom <jhaltom@fileandserve.com>
2018-09-04 17:35:08 -0500

    Escape quotes?

--- src/BoolExpr.Console/Program.cs
+++ src/BoolExpr.Console/Program.cs
@@ -9,19 +9,23 @@ namespace BoolExprNet.Console
         public static void Main(string[] args)
         {
             var ctx = new Context();
+
             var a = ctx.GetVariable("a");
             var b = ctx.GetVariable("b");
             var c = ctx.GetVariable("c");
-            var z = ctx.GetVariable("z");
+            var d = ctx.GetVariable("d");
+            var e = ctx.GetVariable("e");
+            var f = ctx.GetVariable("f");
+            var g = ctx.GetVariable("g");
 
-            var f =
-                    Or(
-                        And(a, Not(b), Not(c)),
-                        And(Not(a), Not(b), c),
-                        And(Not(a), b, Not(c)))
-                .ToDnf().ToCnf();
+            var f1 = Or(a, b);
+            var f2 = Or(c, d);
+            var f3 = Or(e, And(f, g));
+            var fs = And(f1, f2, f3);
 
-            System.Console.WriteLine(f);
+            fs = fs.ToDnf();
+
+            System.Console.WriteLine(fs);
             System.Console.ReadLine();
         }
 
